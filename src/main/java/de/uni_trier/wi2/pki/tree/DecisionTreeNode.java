e542cc1774a1b652930b1e28652ced041578f86d
MrMuhh <84584803+MrMuhh@users.noreply.github.com>
2021-05-22 14:06:11 +0200

    Update DecisionTreeNode.java
    Tree Node class has been created and methods have been initialized

--- src/main/java/de/uni_trier/wi2/pki/tree/DecisionTreeNode.java
+++ src/main/java/de/uni_trier/wi2/pki/tree/DecisionTreeNode.java
@@ -2,24 +2,24 @@ package de.uni_trier.wi2.pki.tree;
 
 import java.util.HashMap;
 
-/**
- * Class for representing a node in the decision tree.
- */
-public class DecisionTreeNode {
 
-    /**
-     * The parent node in the decision tree.
-     */
-    protected DecisionTreeNode parent;
+public class DecisionTreeNode {                     // Class for representing a node in the decision tree.
+    protected DecisionTreeNode parent;              // The parent node in the decision tree.
+    protected int attributeIndex;                   // The attribute index to check.
+    HashMap<String, DecisionTreeNode> splits;       // The checked split condition values and the nodes for these conditions.
 
-    /**
-     * The attribute index to check.
-     */
-    protected int attributeIndex;
+    
+    public DecisionTreeNode(DecisionTreeNode parten, int index) {
+        this.attributeIndex = index;
+        this.parent = parten;
+        splits = new HashMap<>();
+    }
 
-    /**
-     * The checked split condition values and the nodes for these conditions.
-     */
-    HashMap<String, DecisionTreeNode> splits;
+    public int getAttributeIndex() {
+        return attributeIndex;
+    }
 
+    public DecisionTreeNode getParent() {
+        return parent;
+    }
 }
