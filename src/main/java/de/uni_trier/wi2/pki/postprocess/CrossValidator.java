8fa38eca4509b51990ac658c47e1d4d2d15ac891
itsme <s4kimoll@uni-trier.de>
2021-06-04 12:48:24 +0200

    CrossValidator

--- src/main/java/de/uni_trier/wi2/pki/postprocess/CrossValidator.java
+++ src/main/java/de/uni_trier/wi2/pki/postprocess/CrossValidator.java
@@ -5,6 +5,8 @@ import de.uni_trier.wi2.pki.tree.DecisionTreeNode;
 import de.uni_trier.wi2.pki.util.ID3Utils;
 
 import java.sql.Date;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.function.BiFunction;
@@ -29,30 +31,51 @@ public class CrossValidator {
     public static DecisionTreeNode performCrossValidation(List<CSVAttribute[]> dataset, int labelAttribute, BiFunction<List<CSVAttribute[]>, Integer, DecisionTreeNode> trainFunction, int numFolds) {
         int range = dataset.size() / numFolds;                  // calculate the size of one fold
 
+        double[] accuracyArray = new double[numFolds];
+
         for (int i = 0; i < numFolds; i++) {
             int startIndex = range * i;                         
             int endIndex = range * (i+1);
             if (i == numFolds-1) endIndex = dataset.size();
-            List<CSVAttribute[]> trainingData = Stream.concat(dataset.subList(0, startIndex).stream(), dataset.subList(endIndex+1, dataset.size()).stream()).collect(Collectors.toList());
+            double sampleSize = endIndex-startIndex;           // used to calculate accuracy later on
+
+            List<CSVAttribute[]> trainingData = Stream.concat(dataset.subList(0, startIndex).stream(), dataset.subList(endIndex, dataset.size()).stream()).collect(Collectors.toList());
+
             DecisionTreeNode root = trainFunction.apply(trainingData, labelAttribute);
-            for (CSVAttribute[] entry : dataset.subList(startIndex, endIndex))
-                DFS(root, entry);
+
+
+            LinkedList<String> resultList = new LinkedList<>();
+            for (CSVAttribute[] entry : dataset.subList(startIndex, endIndex)){
+                resultList.add(consultTree(root, entry));
+            }
+
+
+            double correctClassification = 0;
+            for (String result : resultList) {
+                if (result.equals(dataset.get(startIndex)[labelAttribute].getCategory().toString())){
+                    correctClassification++;
+                }
+                startIndex++;
+            }
+
+            double accuracy = correctClassification / sampleSize;
+            accuracyArray[i] = accuracy;
         }
+
+        double finalAccuracy = Arrays.stream(accuracyArray).average().orElse(Double.NaN);
+        System.out.println("The learned DecisionTree has an accuracy of " +  finalAccuracy);
         
         return null;
     }
 
-    public static String DFS(DecisionTreeNode node, CSVAttribute[] array) {
-        if (node.getSplits().entrySet().iterator().next().getValue() == null) {
+    public static String consultTree(DecisionTreeNode node, CSVAttribute[] array){
+        int attributeID = node.getAttributeIndex();
+        DecisionTreeNode child = node.getSplits().get(array[attributeID].getCategory().toString());
+
+        if (child == null){
             return node.getSplits().entrySet().iterator().next().getKey();
+        } else {
+            return consultTree(child, array);
         }
-
-        for (Map.Entry<String, DecisionTreeNode> entry : node.getSplits().entrySet()) {
-            int attribute = entry.getValue().getAttributeIndex();
-            if (array[attribute].getCategory() == entry.getKey())
-                return DFS(entry.getValue(), array);
-        }
-
-        return null;
     }
 }
