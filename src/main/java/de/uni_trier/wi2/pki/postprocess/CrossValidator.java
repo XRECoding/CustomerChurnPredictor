d24d9670b622a09ab3d7765115174026537e6e12
MrMuhh <weljo870@outlook.de>
2021-06-04 11:25:47 +0200

    .

--- src/main/java/de/uni_trier/wi2/pki/postprocess/CrossValidator.java
+++ src/main/java/de/uni_trier/wi2/pki/postprocess/CrossValidator.java
@@ -2,8 +2,14 @@ package de.uni_trier.wi2.pki.postprocess;
 
 import de.uni_trier.wi2.pki.io.attr.CSVAttribute;
 import de.uni_trier.wi2.pki.tree.DecisionTreeNode;
+import de.uni_trier.wi2.pki.util.ID3Utils;
+
+import java.sql.Date;
 import java.util.List;
+import java.util.Map;
 import java.util.function.BiFunction;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @SuppressWarnings("rawtypes")
 
@@ -20,10 +26,33 @@ public class CrossValidator {
      * @param trainFunction  the function to train the model with.
      * @param numFolds       the number of data folds.
      */
-    public static DecisionTreeNode performCrossValidation(List<CSVAttribute[]> dataset, int labelAttribute,
-                                                          BiFunction<List<CSVAttribute[]>, Integer, DecisionTreeNode> trainFunction,
-                                                          int numFolds) {
+    public static DecisionTreeNode performCrossValidation(List<CSVAttribute[]> dataset, int labelAttribute, BiFunction<List<CSVAttribute[]>, Integer, DecisionTreeNode> trainFunction, int numFolds) {
+        int range = dataset.size() / numFolds;                  // calculate the size of one fold
+
+        for (int i = 0; i < numFolds; i++) {
+            int startIndex = range * i;                         
+            int endIndex = range * (i+1);
+            if (i == numFolds-1) endIndex = dataset.size();
+            List<CSVAttribute[]> trainingData = Stream.concat(dataset.subList(0, startIndex).stream(), dataset.subList(endIndex+1, dataset.size()).stream()).collect(Collectors.toList());
+            DecisionTreeNode root = trainFunction.apply(trainingData, labelAttribute);
+            for (CSVAttribute[] entry : dataset.subList(startIndex, endIndex))
+                DFS(root, entry);
+        }
+        
         return null;
     }
 
+    public static String DFS(DecisionTreeNode node, CSVAttribute[] array) {
+        if (node.getSplits().entrySet().iterator().next().getValue() == null) {
+            return node.getSplits().entrySet().iterator().next().getKey();
+        }
+
+        for (Map.Entry<String, DecisionTreeNode> entry : node.getSplits().entrySet()) {
+            int attribute = entry.getValue().getAttributeIndex();
+            if (array[attribute].getCategory() == entry.getKey())
+                return DFS(entry.getValue(), array);
+        }
+
+        return null;
+    }
 }
